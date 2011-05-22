968e0126f9fce49e5ee90f6cd62834925b3be096
nohostname <koptein@nhn.koptein.de>
2011-05-22 22:34:32 +0200

    added Piwik script

--- index.php
+++ index.php
@@ -50,6 +50,22 @@
         <div class='footer'>
             Made by <a href="http://www.koptein.de">Christian Koptein</a>  
         </div>
+        <!-- Piwik --> 
+        <script type="text/javascript">
+        var pkBaseURL = (("https:" == document.location.protocol) ?
+        "https://piwik.whatsmyipv6.org/" : "http://piwik.whatsmyipv6.org/");
+        document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js'
+        type='text/javascript'%3E%3C/script%3E"));
+        </script><script type="text/javascript">
+        try {
+            var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
+            piwikTracker.trackPageView();
+            piwikTracker.enableLinkTracking();
+        } catch( err ) {}
+        </script><noscript><p><img
+        src="http://piwik.whatsmyipv6.org/piwik.php?idsite=1" style="border:0"
+        alt="" /></p></noscript>
+        <!-- End Piwik Tracking Code -->
     </body>
 </html>
 
